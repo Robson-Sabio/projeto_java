9069510e70f69b73a075a3666034d9285894abbd
Robson <robson.sabio.ferreira@gmail.com>
2023-03-07 17:32:45 -0300

    textes de laços de repetição e variaveis.

--- src/texte/AreaDeTextes.java
+++ src/texte/AreaDeTextes.java
@@ -1,5 +1,28 @@
 package texte;
 
+import java.util.Scanner;
+
 public class AreaDeTextes {
 
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i<3 ; i++) {
+		int media = 5 ,nota;
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.print("insira a sua nota: ");
+		nota = leitor.nextInt();
+		
+		if (nota > media) {
+			System.out.println("vc passou!");
+		}else if (nota == media) {
+			System.out.println("vc esta de recuperacao!");
+			
+		}else {
+			System.out.println("vc reprovou!");
+		}
+		}
+		System.out.println("fim do programa!!!");
+	}
 }
